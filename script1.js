a0f2edada395413479fed135f967331d3cfa3014
Ameya Patil <ameya.patil1953@gmail.com>
2024-07-14 02:37:16 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,21 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var numField2 = document.getElementById('numfield2');
     var resultField = document.getElementById('resultfield');
     var form = document.getElementById('xisWhatPercentageofy');
+    var clearButton = document.getElementById('clearButton');
 
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
-        if(!numField1.value || !numField2.value){
+        if (!numField1.value || !numField2.value) {
             alert("Please enter values in both fields.");
         } else {
             var x = parseFloat(numField1.value);
             var y = parseFloat(numField2.value);
 
-            if (isNaN(x) || isNaN(y)) {
-                alert("Please enter valid numbers.");
-            } else {
-                var result = (x / y) * 100;
-                resultField.innerText = "Answer: " + result.toFixed(2) + "%";
-            }
+            var result = (x / y) * 100;
+            resultField.innerText = "Answer: " + result.toFixed(2) + "%";
         }
+    });
+
+    clearButton.addEventListener('click', function() {
+        numField1.value = '';
+        numField2.value = '';
+        resultField.innerText = 'Answer = ';
     });
 });
